f80954934c98a222151f49ba7bd476b0a24578fd
John Paul Mintz <johnpaul.mintz@gmail.com>
2019-01-04 20:13:35 -0600

    rps choices fix

--- Rand_Round Generator.py
+++ Rand_Round Generator.py
@@ -5,7 +5,7 @@ print('Enter number of rounds:')
 num_rounds = input()
 choice_dict = {}
 for i in range(int(num_rounds)):
-    choice = random.choice(['Peace', 'War'])#['Rock', 'Paper', 'Scissors'])
+    choice = random.choice(['Rock', 'Paper', 'Scissors'])#['Peace', 'War'])#
     print(str(i+1)+": " +choice)
     choice_dict[i]=choice
 
