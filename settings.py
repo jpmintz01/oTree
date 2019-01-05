7bd1f48dab3185b2464504394d23be6e72fd6c80
John Paul Mintz <johnpaul.mintz@gmail.com>
2019-01-04 19:52:36 -0600

    rem browser bots and add new test room

--- settings.py
+++ settings.py
@@ -86,7 +86,7 @@ SESSION_CONFIGS = [
         'name': 'multi_game_test',
         'display_name': "---> Experiment Start to Finish",
         'num_demo_participants': 10,
-        'use_browser_bots': True,
+        'use_browser_bots': False,
         'num_RPS_rounds': 20,
         'num_PW_rounds': 20,
         'target_RPS_score': 1, #this puts the player's target score at slightly better than average - could add a 'target_human_score and a target_vs_AI_adversary_score or a target_AI_advisor_score if needed/desired'
